3fd996bdfb73e54dc4e2b17b74f432bb6da8f0e2
FarisDaffa <65797160+Faris0520@users.noreply.github.com>
2021-09-18 15:40:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(cors())
 app.use(secure)
 app.use(express.static("public"))
 
-app.use('/', mainrouter)
-app.use('/api', apirouter)
+app.use('/', apirouter)
 
 app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
